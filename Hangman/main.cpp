d76bbfc954c0dd5ea7e433574768ac0f1dfd9301
syedsaad314 <syedsaadbinirfan14@gmail.com>
2026-07-12 08:30:33 +0500

    feat(hangman): anchor application runtime entry points and orchestrators

---
+++ Hangman/main.cpp
@@ -0,0 +1,8 @@
+#include "include/GameEngine.hpp"
+
+int main() {
+    // Instantiate and orchestrate the advanced C++ modular game engine
+    HangmanEngine::GameEngine applicationCoreNode;
+    applicationCoreNode.ExecuteCoreEngineLoop();
+    return 0;
+}
