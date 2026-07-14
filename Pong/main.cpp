af3e1c8a7d6ceb2fc18e61b0bdf014039c590415
syedsaad314 <syedsaadbinirfan14@gmail.com>
2026-07-14 06:41:26 +0500

    feat(pong): anchor execution launch pipelines

---
+++ Pong/main.cpp
@@ -0,0 +1,8 @@
+#include "include/GameEngine.hpp"
+
+int main() {
+    // Fire up Syed Saad Bin Irfan's console architecture
+    PongEngine::GameEngine terminalAppNode;
+    terminalAppNode.ExecuteMainLoop();
+    return 0;
+}
